81bc6242020f0dc53c996a46df00f6a4b6e0cce6
rahul kumar <142343501+CodeSciRahul@users.noreply.github.com>
2024-02-03 19:57:09 +0530

    improve some its ui

--- script.js
+++ script.js
@@ -1,35 +1,62 @@
+let sun=document.querySelector("nav .change-icon i:first-child");
+let moon=document.querySelector("nav .change-icon i:last-child");
+let project=document.querySelectorAll(".Project div");
+console.log(project);
+sun.addEventListener("click",()=>{
+    document.documentElement.style.setProperty("--text-color","black");
+    document.documentElement.style.setProperty("--bg","whitesmoke");
+    document.documentElement.style.setProperty("--bg1"," rgba(255, 255, 255, 0.27)");
+    document.documentElement.style.setProperty("--other-text"," rgb(60, 58, 58)")
+    moon.style.display="block";
+    sun.style.display="none";
+    project.forEach(change => {
+        change.style.backgroundColor="#4f94d0b8";
+        
+    });
+})
+moon.addEventListener("click",()=>{
+    document.documentElement.style.setProperty("--text-color","white");
+    document.documentElement.style.setProperty("--bg","#1b1f24");
+    document.documentElement.style.setProperty("--bg1","#191d21");
+    document.documentElement.style.setProperty("--other-text","gray");
+    moon.style.display="none";
+    sun.style.display="block";
+    project.forEach(change => {
+        change.style.backgroundColor="transparent";
+        
+    });
+})
 let nav=document.querySelector("nav");
 function updatebackground()
 {
-    if(window.scrollY> window.screen.height)
+    if(window.scrollY> ( window.screen.height)/2)
     {
         nav.classList.add('scrolled');
     }
-    else if(window.scrollY<= window.screen.height){
-        nav.classList.remove('scrolled');;
+    else if(window.scrollY<=( window.screen.height)/2){
+        nav.classList.remove('scrolled');
     }
 }
 window.addEventListener("scroll",updatebackground);
 function resetbackground()
 {
-    if(window.scrollY<window.screen.height)
+    if(window.scrollY<( window.screen.height)/2)
     {
         nav.classList.add('back_scrolled');
     }
-    else if(window.scrollY> window.screen.height){
-        nav.classList.remove('back_scrolled');;
+    else if(window.scrollY> ( window.screen.height)/2){
+        nav.classList.remove('back_scrolled');
     }
 }
 window.addEventListener("scroll",resetbackground);
 
 let line_icon = document.querySelector("nav .icon i:first-child");
-let cross_icon = document.querySelector("nav .icon i:last-child")
+let cross_icon = document.querySelector("nav .icon i:last-child");
 line_icon.addEventListener("click", () => {
     let new_nav = document.querySelector("header .list");
     new_nav.style.display = "block";
     line_icon.style.display = "none";
     cross_icon.style.display = "block";
-    line_icon.style.transform = "rotate(360deg)";
 })
 cross_icon.addEventListener("click", () => {
     let new_nav = document.querySelector("header .list");
@@ -37,7 +64,6 @@ cross_icon.addEventListener("click", () => {
     line_icon.style.display = "block";
     cross_icon.style.display = "none";
 })
-
 let marks = document.getElementById("h31").innerText;
 let counter = 0;
 setInterval(() => {
@@ -101,6 +127,7 @@ setInterval(() => {
         marks_4 = counter_4;
     }
 }, 120);
+
 let form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
     event.preventDefault();
